99e4f6f44115bd8125c1a5a972524e3f5f3a5e17
elsahapemalang1-lgtm <elsahapemalang1@gmail.com>
2026-09-09 18:03:50 +0700

    feat: tambah produk ketiga dengan deskripsi yang spesifik

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
   promoButton.textContent = "Promo: Beli 2 gratis tester!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
+  console.log("Promo Teh Saha berhasil ditampilkan.");
 });
